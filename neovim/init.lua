d34b7d07482ba81467fd1d475fa7aa6a6f19cf7e
Anton Babenko <antonwarbeck123@gmail.com>
2021-05-25 02:28:13 +0300

    Added nvim 0.5 configurations

---
+++ neovim/init.lua
@@ -0,0 +1,157 @@
+-------------------- HELPERS -------------------------------
+local cmd = vim.cmd  -- to execute Vim commands e.g. cmd('pwd')
+local fn = vim.fn    -- to call Vim functions e.g. fn.bufnr()
+local g = vim.g      -- a table to access global variables
+local scopes = {o = vim.o, b = vim.bo, w = vim.wo}
+local fmt = string.format
+
+local function opt(scope, key, value)
+  scopes[scope][key] = value
+  if scope ~= 'o' then scopes['o'][key] = value end
+end
+
+local function map(mode, lhs, rhs, opts)
+  local options = {noremap = true}
+  if opts then options = vim.tbl_extend('force', options, opts) end
+  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
+-------------------- PLUGINS -------------------------------
+cmd 'packadd paq-nvim'               -- load the package manager
+local paq = require('paq-nvim').paq  -- a convenient alias
+paq {'savq/paq-nvim', opt = true}    -- paq-nvim manages itself
+paq {'airblade/vim-gitgutter'}
+paq {'joshdick/onedark.vim'}
+paq {'shougo/deoplete-lsp'}
+paq {'shougo/deoplete.nvim', run = fn['remote#host#UpdateRemotePlugins']}
+paq {'nvim-treesitter/nvim-treesitter'}
+paq {'neovim/nvim-lspconfig'}
+paq {'junegunn/fzf', run = fn['fzf#install']}
+paq {'junegunn/fzf.vim'}
+paq {'ojroques/nvim-lspfuzzy'}
+paq {'ojroques/nvim-bufbar'}
+paq {'ojroques/nvim-bufdel'}
+paq {'ojroques/nvim-hardline'}
+paq {'justinmk/vim-dirvish'}
+paq {'lervag/vimtex'}
+paq {'tpope/vim-commentary'}
+
+-------------------- PLUGIN SETUP --------------------------
+-- bufbar
+require('bufbar').setup {show_bufname = 'visible', show_flags = false}
+
+-- bufdel
+map('n', '<leader>w', '<cmd>BufDel<CR>')
+require('bufdel').setup {next = 'alternate'}
+
+-- vimtex
+g['vimtex_quickfix_mode'] = 0
+g['vimtex_view_method'] = 'zathura'
+cmd 'au VimEnter * call deoplete#custom#var("omni", "input_patterns", {"tex": g:vimtex#re#deoplete})'
+
+-- deoplete
+g['deoplete#enable_at_startup'] = 1
+fn['deoplete#custom#option']('ignore_case', false)
+fn['deoplete#custom#option']('max_list', 10)
+
+-- dirvish
+g['dirvish_mode'] = [[:sort ,^.*[\/],]]
+
+-- fugitive and git
+local log = [[\%C(yellow)\%h\%Cred\%d \%Creset\%s \%Cgreen\%as \%Cblue\%an\%Creset]]
+map('n', '<leader>g<space>', ':Git ')
+map('n', '<leader>gd', '<cmd>Gvdiffsplit<CR>')
+map('n', '<leader>gg', '<cmd>Git<CR>')
+map('n', '<leader>gl', fmt('<cmd>term git log --graph --all --format="%s"<CR><cmd>start<CR>', log))
+
+-- hardline
+require('hardline').setup {}
+
+-- fzf
+map('n', '<leader>/', '<cmd>BLines<CR>')
+map('n', '<leader>f', '<cmd>Files<CR>')
+map('n', '<leader>r', '<cmd>Rg<CR>')
+map('n', 's', '<cmd>Buffers<CR>')
+g['fzf_action'] = {
+  ['ctrl-t'] = 'tab split',
+  ['ctrl-s'] = 'split',
+  ['ctrl-v'] = 'vsplit',
+}
+-------------------- OPTIONS -------------------------------
+g['mapleader'] = "\\"
+local indent, width = 4, 80
+cmd('let g:nvcode_termcolors=256')
+-- cmd('colorscheme ' .. O.colorscheme)
+cmd 'colorscheme onedark'
+
+opt('b', 'expandtab', true)                           -- Use spaces instead of tabs
+opt('b', 'shiftwidth', indent)                        -- Size of an indent
+opt('b', 'smartindent', true)                         -- Insert indents automatically
+opt('b', 'tabstop', indent)                           -- Number of spaces tabs count for
+opt('b', 'softtabstop', indent)                       -- Number of spaces tabs count for
+opt('b', 'textwidth', width)                          -- Maximum width of text
+opt('o', 'completeopt', 'menuone,noinsert,noselect')  -- Completion options (for deoplete)
+opt('o', 'hidden', true)                              -- Enable modified buffers in background
+opt('o', 'ignorecase', true)                          -- Ignore case
+opt('o', 'joinspaces', false)                         -- No double spaces with join after a dot
+opt('o', 'scrolloff', 4 )                             -- Lines of context
+opt('o', 'shiftround', true)                          -- Round indent
+opt('o', 'sidescrolloff', 8 )                         -- Columns of context
+opt('o', 'smartcase', true)                           -- Don't ignore case with capitals
+opt('o', 'splitbelow', true)                          -- Put new windows below current
+opt('o', 'splitright', true)                          -- Put new windows right of current
+opt('o', 'termguicolors', true)                       -- True color support
+opt('o', 'wildmode', 'list:longest')                  -- Command-line completion mode
+opt('w', 'colorcolumn', tostring(width))              -- Line length marker
+opt('w', 'cursorline', true)                          -- Highlight cursor line
+opt('w', 'list', true)                                -- Show some invisible characters (tabs...)
+opt('w', 'number', true)                              -- Print line number
+opt('w', 'relativenumber', true)                      -- Relative line numbers
+opt('w', 'signcolumn', 'yes')                         -- Show sign column
+opt('w', 'wrap', false)                               -- Disable line wrap
+
+-------------------- MAPPINGS ------------------------------
+map('', '<leader>c', '"+y')       -- Copy to clipboard in normal, visual, select and operator modes
+map('i', '<C-u>', '<C-g>u<C-u>')  -- Make <C-u> undo-friendly
+map('i', '<C-w>', '<C-g>u<C-w>')  -- Make <C-w> undo-friendly
+
+map('i', 'jj', '<ESC>')
+map('n', '<C-l>', '<cmd>nohlsearch<CR>')
+
+--- <tabs>
+map('n', '<C-w>T', '<cmd>tabclose<CR>')
+map('n', '<C-w>t', '<cmd>tabnew<CR>')
+
+-- <Tab> to navigate the completion menu
+map('i', '<S-Tab>', 'pumvisible() ? "\\<C-p>" : "\\<Tab>"', {expr = true})
+map('i', '<Tab>', 'pumvisible() ? "\\<C-n>" : "\\<Tab>"', {expr = true})
+
+map('n', '<C-l>', '<cmd>nohlsearch<CR>') -- Clear highlights
+map('n', '<leader>o', 'm`o<Esc>``')  -- Insert a newline in normal mode
+
+-------------------- TREE-SITTER ---------------------------
+local ts = require 'nvim-treesitter.configs'
+ts.setup {ensure_installed = 'maintained', highlight = {enable = true}}
+
+-------------------- LSP -----------------------------------
+local lsp = require 'lspconfig'
+local lspfuzzy = require 'lspfuzzy'
+
+-- For ccls we use the default settings
+lsp.ccls.setup {}
+-- root_dir is where the LSP server will start: here at the project root otherwise in current folder
+lsp.pyls.setup {root_dir = lsp.util.root_pattern('.git', fn.getcwd())}
+lspfuzzy.setup {}  -- Make the LSP client use FZF instead of the quickfix list
+
+map('n', '<space>,', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>')
+map('n', '<space>;', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>')
+map('n', '<space>a', '<cmd>lua vim.lsp.buf.code_action()<CR>')
+map('n', '<space>d', '<cmd>lua vim.lsp.buf.definition()<CR>')
+map('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>')
+map('n', '<space>h', '<cmd>lua vim.lsp.buf.hover()<CR>')
+map('n', '<space>m', '<cmd>lua vim.lsp.buf.rename()<CR>')
+map('n', '<space>r', '<cmd>lua vim.lsp.buf.references()<CR>')
+map('n', '<space>s', '<cmd>lua vim.lsp.buf.document_symbol()<CR>')
+
+-------------------- COMMANDS ------------------------------
+cmd 'au TextYankPost * lua vim.highlight.on_yank {on_visual = false}'  -- disabled in visual mode
