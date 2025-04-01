faa2d10985dfae70b6481dc8303f4949bde664ce
Anton Babenko <antonwarbeck123@gmail.com>
2025-04-01 15:12:45 +0300

    Updated version of telescope

--- neovim/lua/antonbabenko/plugins.lua
+++ neovim/lua/antonbabenko/plugins.lua
@@ -20,7 +20,7 @@ packer.startup(function(use)
   -- Packer can manage itself
   use { 'wbthomason/packer.nvim' }
   use {
-      'nvim-telescope/telescope.nvim', tag = '0.1.4',
+      'nvim-telescope/telescope.nvim', tag = '0.1.8',
       -- or                            , branch = '0.1.x',
       requires = { {'nvim-lua/plenary.nvim'} }
   }
